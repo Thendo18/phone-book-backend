f69d4540684be5ff2955d4acac01f3029ba06a5f
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-09-02 23:02:11 +0200

    functions setup

--- src/phonebook/phonebook.controller.ts
+++ src/phonebook/phonebook.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { PhonebookService } from './phonebook.service';
+import { Request } from 'express';
 
 @Controller('phonebook')
-export class PhonebookController {}
+export class PhonebookController {
+
+	constructor(private __phonebook: PhonebookService) {}
+
+	@Get('search')
+	async searchProduct(@Req() req: Request){
+		return this.__phonebook.searchPhonebookByNameOrNumber({});
+	}
+
+	@Post()
+	async addNewPhonebook(@Body() phonebook : { phone: number, name: string }){
+		return await this.__phonebook.addNewPhonebookToTheDatabase(phonebook);
+	}
+
+	@Get()
+	async getAllPhonebooks(){
+		return await this.__phonebook.getAllPhonebooksFromTheDatabase();
+	}
+
+	@Get('/:phonebookId')
+	async getOnePhonebook(@Param('phonebookId') phonebookId: string ){
+		return await this.__phonebook.getOnePhonebookFromTheDatabase(phonebookId);
+	}
+
+	@Patch('/:phonebookId')
+	async updateOnePhonebook(@Param('phonebookId') phonebookId: string, @Body() phonebook : { phone: number, name: string }){
+		return await this.__phonebook.updateOnePhonebookFromTheDatabase(phonebookId, phonebook);
+	}
+
+	@Delete('/:phonebookId')
+	async deleteOnePhonebook(@Param('phonebookId') phonebookId: string ){
+		return await this.__phonebook.deleteOnePhonebookFromTheDatabase(phonebookId);
+	}
+}
--- src/phonebook/phonebook.service.ts
+++ src/phonebook/phonebook.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Phonebook } from './phonebook-model-shema';
 
 @Injectable()
-export class PhonebookService {}
+export class PhonebookService {
+
+	constructor(@InjectModel('Phonebook') private readonly __phonebookModel: Model<Phonebook> ){}
+
+	async addNewPhonebookToTheDatabase(phonebook: any){
+		return "Adding Phone Book...";
+	}
+
+	async getAllPhonebooksFromTheDatabase(){
+		return "Getting All Phone Books...";
+	}
+
+	async getOnePhonebookFromTheDatabase(phonebookId: string){
+		return "Getting One Phone Book...";
+	}
+
+	async updateOnePhonebookFromTheDatabase(phonebookId: string, phonebook: any){
+		return "Updating One Phone Book...";
+	}
+
+	async deleteOnePhonebookFromTheDatabase(phonebookId: string){
+		return "Deleting One Phone Book...";
+	}
+
+	searchPhonebookByNameOrNumber(options: any){
+		return "Searching Phone Book...";
+	}
+}
