6c2a95e289f17ee25cd61abeab57d156d940f355
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-09-02 23:05:03 +0200

    functions setup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+	
+	app.enableCors({
+		origin: ['http://localhost:4200'],
+		credentials: true
+	});
+	app.setGlobalPrefix('api');
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
