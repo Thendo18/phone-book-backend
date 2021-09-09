f1c13e8754067766716de64cdd8ed94941273521
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-09-09 18:20:58 +0200

    email not unique

--- src/phonebook/phonebook-model-shema.ts
+++ src/phonebook/phonebook-model-shema.ts
@@ -2,8 +2,8 @@ import * as mongoose from 'mongoose';
 
 export const PhonebookSchema = new mongoose.Schema({
     phone: { type: Number, required: [ true, 'Phone number is required' ] , unique: [ true, "Phone number already exists" ] },
-	email: { type: String, unique: [ true, "Email already exists" ] },
 	name: { type: String, required: [ true, 'Name is required' ] },
+	email: { type: String },
 });
 
 export interface Phonebook extends mongoose.Document{
