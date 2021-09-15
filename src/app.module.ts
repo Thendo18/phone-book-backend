a9a7ecf94113b6d4ff0e9036cce8aa44a73bb3c2
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-09-15 09:09:52 +0200

    remove database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		// MongooseModule.forRoot("mongodb+srv://nolan__kgotso:Nolan123@phonebook.gu113.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { autoCreate: true, }),
 		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
 		PhonebookModule,
 	],
