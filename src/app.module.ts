fd68cd31d7d48dba65484f266afd177caee95207
@Thendo <makherana@thedigitalacademy.co.za>
2021-09-15 11:38:40 +0200

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://Thendo:12345@cluster0.uchpx.mongodb.net/phonebook?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
